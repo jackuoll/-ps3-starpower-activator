4527c4a8762e6ed69f8980772cdff04371fae1d9
Jack Ward <jackward84@gmail.com>
2023-01-30 23:28:15 +1300

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# GH3 controller starpower activator for PC
+
+This program listens to your guitar input and simulates pressing a key on your keyboard when you tilt the guitar. Although I'd prefer you compile and run it yourself, I published an executable version [here](https://drive.google.com/file/d/1kpMWko2No_Ij9fT4Y6CQ-tISG5UoWpcT/view?usp=sharing) that you can download.
+
+## Instructions for Clone Hero
+
+1. First, configure the script with whatever key you want it to push when you tilt the guitar. The default is 's', so if you're fine with that, you don't need to change it. If not, you must change `static uint StarPowerKey = 0x53;` to the [valid keycode](https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes).
+2. Your Clone Hero window should have the name "Clone Hero". The window name that is in the foreground of your PC is used to determine whether or not the Starpower key should be hit or not, to avoid random inputs being pressed when Clone Hero is not active. If your game does not have the window name "Clone Hero", then you will need to change it in the script (`static readonly string GameWindowName = "Clone Hero";`)
+3. It's possible your Guitar may not have the same vendor ID/product ID as mine. You can try fiddle with it in `static UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(0x12ba, 0x0100);`, but please don't bother asking me for support regarding this. Good luck!
+
+### Notes 
+
+* I threw this together in about an hour and obviously I don't have a plethora of GH hardware to test it with. I tested it with my own GH3 WT wireless guitars.
+* I do not know if this is the same way the official GH games recognise tilt and don't really care to spend time figuring out the best way to detect tilt. Take it or leave it. There's some notes in the script itself you can use to debug if you care to.
+* I don't really care to develop this any more unless maybe you have a really cool idea.
+* If it doesn't work, no I'm not going to support it unless it personally affects my gameplay.
---
+++ ps3-starpower-activator/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using LibUsbDotNet;
+using LibUsbDotNet.LibUsb;
+using LibUsbDotNet.Main;
+
+namespace GH3Controller
+{
+    internal class StarpowerActivator
+    {
+        // if for some reason your game window isn't called "clone hero", you'll need to change it here. case sensitive.
+        static readonly string GameWindowName = "Clone Hero";
+
+        // if your guitar uses a different vendorid/productid you'll need to change it here. i only know my own guitars.
+        static UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(0x12ba, 0x0100);
+
+        // set the key to use in clone hero to activate starpower. get the key from https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+        // the one already set is 's'
+        static uint StarPowerKey = 0x53;
+
+        static IUsbDevice PS3Guitar;
+        
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private static bool GameIsForeground()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                var foregroundWindowName = Buff.ToString();
+                //Console.WriteLine($"window: {foregroundWindowName}");
+                return foregroundWindowName == GameWindowName;
+            }
+            return false;
+        }
+
+        public static void Main(string[] args)
+        {
+            Error ec = Error.Success;
+
+            using (UsbContext context = new UsbContext())
+            {
+                try
+                {
+                    // Find and open the usb device.
+                    PS3Guitar = (UsbDevice)context.Find(MyUsbFinder);
+                    
+                    // If the device is open and ready
+                    if (PS3Guitar == null) throw new Exception("Device Not Found.");
+                    
+                    PS3Guitar.Open();
+
+                    // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                    // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                    // 'wholeUsbDevice' variable will be null indicating this is 
+                    // an interface of a device; it does not require or support 
+                    // configuration and interface selection.
+                    IUsbDevice wholeUsbDevice = PS3Guitar as IUsbDevice;
+                    if (!ReferenceEquals(wholeUsbDevice, null))
+                    {
+                        // This is a "whole" USB device. Before it can be used, 
+                        // the desired configuration and interface must be selected.
+
+                        // Select config #1
+                        wholeUsbDevice.SetConfiguration(1);
+
+                        // Claim interface #0.
+                        wholeUsbDevice.ClaimInterface(0);
+                    }
+
+                    // open read endpoint 1.
+                    var reader = PS3Guitar.OpenEndpointReader(ReadEndpointID.Ep01);
+
+
+                    byte[] readBuffer = new byte[28];
+                    bool? lastTiltState = null;
+                    int starPowerInc = 0;
+                    while (ec == Error.Success)
+                    {
+                        int bytesRead;
+
+                        // If the device hasn't sent data in the last 5 seconds,
+                        // a timeout error (ec = IoTimedOut) will occur. 
+                        ec = reader.Read(readBuffer, 5000, out bytesRead);
+
+                        if (bytesRead == 0) throw new Exception(string.Format("{0}:No more bytes!", ec));
+                        var isTilted = readBuffer[20] == 2 && readBuffer[22] == 2 && readBuffer[24] == 2;
+                        var activateStarPower = false;
+
+                        /* 
+                         * 19, 20, 21, 22, 23, 24 are related to guitar tilt
+                         * 19 and 20 - not really sure, it seems to stick, but [20] == 2 seems indicative of the guitar being around 45 deg to the vertical.
+                         * 21 and 22 - likewise, i don't care enough to test but [22] == 2 seems indicative of guitar being vertical
+                         * 23 and 24 - these seem related to front tilt and probably are not used for starpower
+                         */
+
+                        if(lastTiltState != null)
+                        {
+                            activateStarPower = isTilted && !(bool)lastTiltState;
+                        }
+
+                        
+                        lastTiltState = isTilted;
+                        
+                        if (activateStarPower && GameIsForeground())
+                        {
+
+                            const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+                            keybd_event((byte)StarPowerKey, 0, KEYEVENTF_EXTENDEDKEY | 0, 0);
+                            Console.WriteLine($"Activating star power {++starPowerInc}!");
+                        }
+
+                        // you can debug your guitar inputs here if you care to
+                        // just change the loop to include i=0 to i<28 and then uncomment the writeline
+                        // the guitars i tested were gh3 les paul and the WT guitars, they both had the same bytearray read of len 28
+                        var sb = new StringBuilder("new byte[] { ");
+                        for(var i=19; i <= 22; i++)
+                        {
+                            var b = readBuffer[i];
+                            sb.Append($"[{i}]: {b} ,");
+                        }
+                        sb.Append("}");
+                        //Console.WriteLine(sb.ToString());
+
+                    }
+
+                    Console.WriteLine("\r\nDone!\r\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine((ec != Error.Success ? ec + ":" : String.Empty) + ex.Message);
+                }
+                finally
+                {
+                    if (PS3Guitar != null)
+                    {
+                        if (PS3Guitar.IsOpen)
+                        {
+                            // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                            // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                            // 'wholeUsbDevice' variable will be null indicating this is 
+                            // an interface of a device; it does not require or support 
+                            // configuration and interface selection.
+                            IUsbDevice wholeUsbDevice = PS3Guitar as IUsbDevice;
+                            if (!ReferenceEquals(wholeUsbDevice, null))
+                            {
+                                // Release interface #0.
+                                wholeUsbDevice.ReleaseInterface(0);
+                            }
+
+                            PS3Guitar.Close();
+                        }
+                        PS3Guitar = null;
+                    }
+
+                    // Wait for user input..
+                    Console.WriteLine("bye!");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
